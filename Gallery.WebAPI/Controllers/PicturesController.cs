0488c89726b83b741f49a9206b0b51112ef26a9e
srdjan.ruljic <srdjan.ruljic@gmail.com>
2020-08-15 09:24:17 +0200

    Serbian fix for displaying file upload button

--- Gallery.WebAPI/Controllers/PicturesController.cs
+++ Gallery.WebAPI/Controllers/PicturesController.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Gallery.BLL.Interfaces;
 using Gallery.DTO;
 using Gallery.WebAPI.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Gallery.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PicturesController : ControllerBase
     {
         private readonly IMapper _mapper;
@@ -30,7 +29,7 @@ namespace Gallery.WebAPI.Controllers
 
         [HttpPost, DisableRequestSizeLimit]
         [Route("")]
-        [Authorize]
+
         public async Task<IActionResult> Insert(PictureViewModel model)
         {
             long id = 0;
@@ -49,9 +48,10 @@ namespace Gallery.WebAPI.Controllers
 
         [HttpPost]
         [Route("search")]
+        [AllowAnonymous]
         public async Task<IActionResult> Search(PictureSearchViewModel model)
         {
-            List<PictureViewModel> pictures = _mapper.Map<List<PictureViewModel>>(await _picturesBusiness.Search(model.Name, 
+            List<PictureViewModel> pictures = _mapper.Map<List<PictureViewModel>>(await _picturesBusiness.Search(model.Name,
                                                                                                                  model.CategoryId));
 
             return Ok(pictures);
