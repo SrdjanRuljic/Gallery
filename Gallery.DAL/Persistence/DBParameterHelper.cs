065fe99de2176db566bdade32a52f8acb1e180e4
Srdjan Ruljic <srdjan.ruljic@gmail.com>
2020-10-16 16:56:38 +0200

    Added methode AddParameterFromModel

---
+++ Gallery.DAL/Persistence/DBParameterHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Text;
+
+namespace Gallery.DAL.Persistence
+{
+    public static class DBParameterHelper
+    {
+        public static void AddParameterFromModel<T>(SqlParameterCollection collection, T model)
+        {
+            foreach (PropertyInfo item in typeof(T).GetProperties())
+            {
+                if (item.Name == "Id")
+                {
+                    collection.AddWithValue("@Id", 0);
+                }
+
+                collection.AddWithValue(String.Format("@{0}", item.Name),
+                                        model?.GetType().GetProperty(item.Name)?.GetValue(model, null));
+            }
+        }
+    }
+}
