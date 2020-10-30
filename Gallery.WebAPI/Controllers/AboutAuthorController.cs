b99d484ee2d1f0170bee3b6654d74c8e846d1944
srdjan.ruljic <srdjan.ruljic@gmail.com>
2020-10-30 12:52:54 +0100

    About Author Api

---
+++ Gallery.WebAPI/Controllers/AboutAuthorController.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using Gallery.BLL.Interfaces;
+using Gallery.DTO;
+using Gallery.WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Gallery.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AboutAuthorController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IAboutAuthorBusiness _aboutAuthorBusiness;
+
+        public AboutAuthorController(IMapper mapper,
+                                     IAboutAuthorBusiness aboutAuthorBusiness)
+        {
+            _mapper = mapper;
+            _aboutAuthorBusiness = aboutAuthorBusiness;
+        }
+
+        #region [GET]
+
+        [HttpGet]
+        [Route("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetById(long id)
+        {
+            AboutAuthorViewModel author = _mapper.Map<AboutAuthorViewModel>(await _aboutAuthorBusiness.GetById(id));
+
+            return Ok(author);
+        }
+
+        #endregion
+
+        #region [POST]
+
+        [HttpPost, DisableRequestSizeLimit]
+        [Route("")]
+
+        public async Task<IActionResult> Insert(AboutAuthorViewModel model)
+        {
+            long id = await _aboutAuthorBusiness.UploadAndInsert(_mapper.Map<AboutAuthorDTO>(model));
+
+            return Ok(id);
+        }
+
+        #endregion [POST]
+
+        #region [PUT]
+
+        [HttpPut]
+        [Route("")]
+        public async Task<IActionResult> Update(AboutAuthorViewModel model)
+        {
+            bool isUpdated = false;
+
+            await _aboutAuthorBusiness.Update(_mapper.Map<AboutAuthorDTO>(model));
+
+            return Ok(isUpdated);
+        }
+
+        #endregion
+    }
+}
---
+++ Gallery.WebAPI/Models/AboutAuthorViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Gallery.WebAPI.Models
+{
+    public class AboutAuthorViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public string Content { get; set; }
+        public string Extension { get; set; }
+
+    }
+}
