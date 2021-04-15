9370fbe26cb9c012bf0629d1f3e9ea0edba20e12
Srdjan Ruljic <srdjan.ruljic@gmail.com>
2021-04-15 08:42:07 +0200

    myslq search

--- Application/Products/Queries/Search/SearchProductsQueryHandler.cs
+++ Application/Products/Queries/Search/SearchProductsQueryHandler.cs
@@ -1,9 +1,10 @@
-﻿using Application.Common.Interfaces;
+﻿using Application.Common.Helpers;
+using Application.Common.Interfaces;
 using AutoMapper;
-using Domain.Entities;
+using AutoMapper.QueryableExtensions;
 using MediatR;
-using Nest;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,41 +12,32 @@ namespace Application.Products.Queries.Search
 {
     public class SearchProductsQueryHandler : IRequestHandler<SearchProductsQuery, SearchProductsViewModel>
     {
-        private readonly IGalleryDbContext _context;
         private readonly IMapper _mapper;
         private readonly IGalleryMySqlDbContext _myslqContext;
-        private readonly IElasticClient _elasticClient;
 
-        public SearchProductsQueryHandler(IGalleryDbContext context,
-                                         IMapper mapper,
-                                         IGalleryMySqlDbContext myslqContext,
-                                         IElasticClient elasticClient)
+        public SearchProductsQueryHandler(IMapper mapper,
+                                          IGalleryMySqlDbContext myslqContext)
         {
-            _context = context;
             _mapper = mapper;
             _myslqContext = myslqContext;
-            _elasticClient = elasticClient;
         }
 
         public async Task<SearchProductsViewModel> Handle(SearchProductsQuery command, CancellationToken cancellationToken)
         {
-            ISearchResponse<Product> response = await _elasticClient.SearchAsync<Product>(
-                s => s.Query(q => q.QueryString(d => d.Query(command.Name))));
+            IQueryable<SearchProductsQueryResult> list = _myslqContext.Products.Where(x => string.IsNullOrEmpty(command.Name) ?
+                                                                                           true :
+                                                                                           x.Name.Contains(command.Name))
+                                                                               .ProjectTo<SearchProductsQueryResult>(_mapper.ConfigurationProvider);
 
-            //IQueryable<SearchProductsQueryResult> list = _myslqContext.Products.Where(x => string.IsNullOrEmpty(command.Name) ?
-            //                                                                               true :
-            //                                                                               x.Name.Contains(command.Name))
-            //                                                                   .ProjectTo<SearchProductsQueryResult>(_mapper.ConfigurationProvider);
-
-            //PaginatedList<SearchProductsQueryResult> paginatedList = await PaginatedList<SearchProductsQueryResult>.CreateAsync(list.AsNoTracking(),
-            //                                                                                                                    command.PageNumber ?? 1,
-            //                                                                                                                    command.PageSize);
+            PaginatedList<SearchProductsQueryResult> paginatedList = await PaginatedList<SearchProductsQueryResult>.CreateAsync(list.AsNoTracking(),
+                                                                                                                                command.PageNumber ?? 1,
+                                                                                                                                command.PageSize);
 
             return new SearchProductsViewModel
             {
-                Products = _mapper.Map<List<SearchProductsQueryResult>>(response.Documents),
-                TotalPages = response.Total,
-                PageIndex = command.PageNumber
+                Products = paginatedList,
+                TotalPages = paginatedList.TotalPages,
+                PageIndex = paginatedList.PageIndex
             };
         }
     }
