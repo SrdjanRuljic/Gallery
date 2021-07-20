492c1e8461e12c59597c3f4f1fbaa6ca3675be60
Srdjan Ruljic <srdjan.ruljic@gmail.com>
2021-07-20 10:41:05 +0200

    logger

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Common.Behaviours;
 using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Application
 {
@@ -15,6 +11,7 @@ namespace Application
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
 
             return services;
         }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,14 +2,11 @@ using Application.System.Commands.SeedData;
 using MediatR;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -33,8 +30,10 @@ namespace WebAPI
                     IMediator mediator = services.GetRequiredService<IMediator>();
                     await mediator.Send(new SeedDataCommand(), CancellationToken.None);
                 }
-                catch ( Exception ex)
+                catch (Exception ex)
                 {
+                    ILogger<Program> logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
                 }
             }
 
